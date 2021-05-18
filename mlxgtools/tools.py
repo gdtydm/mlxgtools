dee925f3fad5f4c00b6fe07f9ab26419827c552b
liqing05 <liqing05@corp.netease.com>
2021-05-18 10:47:24 +0800

    add multprocess

--- mlxgtools/tools.py
+++ mlxgtools/tools.py
@@ -2,6 +2,8 @@
 import os 
 import json 
 import copy 
+from multiprocessing import Pool
+from datetime import datetime 
 
 
 class ArgsClass(object):
@@ -60,4 +62,53 @@ def get_gcs_path_of_kaggle_data(data_name, is_private=False):
     GCS_DS_PATH = KaggleDatasets().get_gcs_path(data_name)	
     return GCS_DS_PATH 
 
+
+def multiprocess_fnc_without_res(fn, args, core):
+    parallel_cnt = 0
+    parallel_num = len(args)
+    def call_back(rst):
+        parallel_cnt += 1
+        print('%s, %d / %d done!' % (datetime.datetime.now(), parallel_cnt, parallel_num))
+
+    pool = Pool(core)
+    for arg in args:
+        pool.apply_async(fn, args=arg,  callback=call_back)
+    pool.close()
+    pool.join()
+
+def multprocess_fnc(fn, args, core):
+    parallel_cnt = 0
+    parallel_num = len(args)
+    def call_back(rst):
+        parallel_cnt += 1
+        print('%s, %d / %d done!' % (datetime.datetime.now(), parallel_cnt, parallel_num))
+
+
+    serverid_num = len(args)
+    record_data = []
+    record_num = 0 # ###成功运行
+    empty_num = 0   # ###成功运行但是结果为空
+    error_num = 0  # ###运行失败
+    pool = Pool(4)
+    for arg in args:
+        record_data.append(pool.apply_async(fn, args=arg, callback=call_back))
+    pool.close()
+    pool.join()
+
+    for i in record_data:
+        # ####运行无误
+        if i.get()[1] == 0:
+            record_num += 1
+        # ####运行无误，但是无结果
+        if i.get()[1] == 0 and i.get() == 0:
+            empty_num += 1
+
+        # ####运行错误
+        if i.get()[1] == 1 :
+            error_num += 1
+
+    print(f'[INFO] all num: {serverid_num}, record num: {record_num}, empty num: {empty_num}, error num: {error_num}')
+
+    res = [i.get()[0] for i in record_data] 
+    return res 
 
